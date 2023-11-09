e33c3cf64a5ddb17098f9cff7a48a16d8bfea267
LeoGuoDNAS <lguo@wearetheone.com>
2023-11-09 13:13:30 -0500

    init setup

---
+++ agent.py
@@ -0,0 +1,60 @@
+from langchain.agents import initialize_agent, AgentType
+from langchain.callbacks import StreamlitCallbackHandler
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+import os
+import openai
+from dotenv import load_dotenv
+from tools import dnas_qa_lc_tool, sop_lc_tool, sampro_webhelp_lc_tool, policy_qa_lc_tool
+
+load_dotenv()
+openai.api_key = os.getenv('api_key')
+os.environ['OPENAI_API_KEY'] = os.getenv('api_key')
+
+# Define LLM to be used
+llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", streaming=True)
+
+# Define LangChain Agent
+agent = initialize_agent(
+    [
+        dnas_qa_lc_tool,
+        sop_lc_tool,
+        sampro_webhelp_lc_tool,
+        policy_qa_lc_tool
+    ],
+    llm=llm,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=True,
+    handle_paring_errors=True
+)
+
+# agent.run("What is the vacation policy? How many days do we get off? And what are they?")
+
+st.set_page_config(page_title="Day & Nite Chatbot", page_icon="🤖")
+st.title("☀️🌑🤖 Day & Nite Chatbot")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {
+            "role": "assistant", 
+            "content": "How can I help you today?"
+        },
+        {
+            "role": "assistant", 
+            "content": "I can answer questions about 1. Day & Nite's company general information, 2. Standard of Procedure, 3. Day & Nite policeis, and 4. SamPro WebHelp."
+        }
+    ]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"], avatar="🤖").write(msg["content"])
+
+if prompt := st.chat_input(placeholder="Start typing..."):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user", avatar="🤔").write(prompt)
+    with st.chat_message("assistant", avatar="🤖"):
+        st.write("☀️🌑🤖 Thinking...")
+        st_callback_handler = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+        response = agent.run(prompt, callbacks=[st_callback_handler])
+        st.session_state.messages.append({"role": "assistant", "content": response})
+        st.write(response)
+    
---
+++ tools.py
@@ -0,0 +1,79 @@
+import pinecone
+from llama_index.vector_stores import PineconeVectorStore
+from llama_index.embeddings.openai import OpenAIEmbedding
+import os
+from llama_index.tools import BaseTool, FunctionTool, QueryEngineTool, ToolMetadata
+from llama_index import VectorStoreIndex, SimpleDirectoryReader, ServiceContext, GPTVectorStoreIndex, download_loader
+from dotenv import load_dotenv
+import openai
+
+# Load environment variables from .env file
+load_dotenv()
+openai.api_key = os.getenv('api_key')
+os.environ['PINECONE_API_KEY'] = os.getenv('pinecone_api_key')
+os.environ['PINECONE_ENVIRONMENT'] = os.getenv('pinecone_env')
+
+# Create Pinecone vector store and query engine
+sampro_webhelp_index_name = 'dnas-sops'
+pinecone.init(
+    api_key=os.environ['PINECONE_API_KEY'],
+    environment=os.environ['PINECONE_ENVIRONMENT']
+)
+sampro_webhelp_index = pinecone.Index(sampro_webhelp_index_name)
+sampro_webhelp_vector_store = PineconeVectorStore(pinecone_index=sampro_webhelp_index)
+pinecone_embed_model = OpenAIEmbedding(model='text-embedding-ada-002', embed_batch_size=100)
+sampro_webhelp_service_context = ServiceContext.from_defaults(embed_model=pinecone_embed_model)
+sampro_webhelp_query_index = GPTVectorStoreIndex.from_vector_store(
+    vector_store=sampro_webhelp_vector_store,
+    service_context=sampro_webhelp_service_context
+)
+sampro_webhelp_query_engine = sampro_webhelp_query_index.as_query_engine()
+
+# Sampro web help qa tool
+sampro_webhelp_tool = QueryEngineTool(
+    query_engine=sampro_webhelp_query_engine,
+    metadata=ToolMetadata(
+        name="sampro_web_help_qa_tool",
+        description="Can answer questions related to SamPRO ERP software. Trouble-shooting, user manuals, and general information about SamPRO ERP."
+    )
+)
+sampro_webhelp_lc_tool = sampro_webhelp_tool.to_langchain_tool()
+
+# DNAS QA Tool
+dnas_qa_documents = SimpleDirectoryReader("./knowledgeBase/dnas/").load_data()
+dnas_qa_index = VectorStoreIndex.from_documents(dnas_qa_documents)
+dnas_qa_query_engine = dnas_qa_index.as_query_engine()
+dnas_qa_tool = QueryEngineTool(
+    query_engine=dnas_qa_query_engine,
+    metadata=ToolMetadata(
+        name="company_factual_qa_tool",
+        description="Information about Day & Nite All Service. Contains information such as contacts, service, history, mission, vision, and DNAS Connect."
+    )
+)
+dnas_qa_lc_tool = dnas_qa_tool.to_langchain_tool()
+
+# SOP QA Tool
+sop_documents = SimpleDirectoryReader("./knowledgeBase/sops/").load_data()
+sop_index = VectorStoreIndex.from_documents(sop_documents)
+sop_query_engine = sop_index.as_query_engine()
+sop_tool = QueryEngineTool(
+    query_engine=sop_query_engine,
+    metadata=ToolMetadata(
+        name="standard_of_procedure_qa_tool",
+        description="Standard of procedures (SOPs) of Day & Nite All Service. Include SOPs for on/off boarding, management, procurement, hr, IT, etc."
+    )
+)
+sop_lc_tool = sop_tool.to_langchain_tool()
+
+# Policy QA Tool
+policy_qa_documents = SimpleDirectoryReader("./knowledgeBase/policies/").load_data()
+policy_qa_index = VectorStoreIndex.from_documents(policy_qa_documents)
+policy_qa_query_engine = policy_qa_index.as_query_engine()
+policy_qa_tool = QueryEngineTool(
+    query_engine=policy_qa_query_engine,
+    metadata=ToolMetadata(
+        name="policy_qa_tool",
+        description="Information about Day & Nite All Service policies. Includes office manual, vacation policy, and employee handbook."
+    )
+)
+policy_qa_lc_tool = policy_qa_tool.to_langchain_tool()
